57458f4287785619e4a41a4c9ab777153f2857f1
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-09-17 17:32:55 +0100

    managed to make the slime animate when it grows

--- js/main.js
+++ js/main.js
@@ -10,11 +10,14 @@ var modHeight = 1;
 
 ($slimeFood).click(function(){
 
-  $($slime).width( modWidth);
+  ($slime).width( modWidth);
   modWidth += 0.1;
 
-  $($slime).height( modHeight);
+  ($slime).height( modHeight);
   modHeight += 0.1;
+	($slime).addClass('animated jello').delay(500).queue(function(){
+		($slime).removeClass('animated jello'). dequeue();
+	})
 
 })
 

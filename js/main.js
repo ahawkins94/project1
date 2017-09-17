49480460c86c16b919a3a8e472a3b7210b983862
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-09-17 18:56:01 +0100

    added a scoring system that is easy to correlate with the size of the slime

--- js/main.js
+++ js/main.js
@@ -5,19 +5,24 @@ var $slimeFood = $("#slime-food");
 var sLeft = $slime.position();
 var modWidth = 1;
 var modHeight = 1;
+var score = 0; 
 
 
 
 ($slimeFood).click(function(){
 
-  ($slime).width( modWidth);
-  modWidth += 0.1;
+	($slime).width( modWidth);
+	modWidth += 0.1;
 
-  ($slime).height( modHeight);
-  modHeight += 0.1;
+	($slime).height( modHeight);
+	modHeight += 0.1;
 	($slime).addClass('animated jello').delay(500).queue(function(){
 		($slime).removeClass('animated jello'). dequeue();
 	})
+	score ++;
+    $('#score').html(score);
+
+
 
 })
 

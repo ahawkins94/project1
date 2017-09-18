641b30bf5563a69bd032299f123d7fb704899025
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-09-18 12:14:32 +0100

    changed the way workers generate slimes, sorted commonly used code into functions. Now multiple workers will increase the speed of slime generation

--- js/main.js
+++ js/main.js
@@ -1,38 +1,41 @@
-console.log('help me')
 
 var $slime = $("#slime");
 var $slimeFood = $("#slime-food");
 var sLeft = $slime.position();
+var $workers = $(".workers")
 var modWidth = 1;
 var modHeight = 1;
 
-
-
-($slimeFood).click(function(){
+function addSize(){
 
 	($slime).width( modWidth);
 	modWidth += 0.1;
 
 	($slime).height( modHeight);
 	modHeight += 0.1;
+}
+
+function updateScore(){
+	$('#score').html(Math.round( $slime.width() * 10 ) / 10);	
+}
+
+($slimeFood).click(function(){
+
+	addSize();
 	($slime).addClass('animated jello').delay(500).queue(function(){
 		($slime).removeClass('animated jello'). dequeue();
 	})
-	$('#score').html(Math.round( $slime.width() * 10 ) / 10);
-
+	updateScore();
 })
 
-// if ($slime hasClass("basic-worker")){
 
-// 	setInterval(function(){
+setInterval(function(){
+	for (i = 0; i < $workers.find(".basic-worker").length; i++){
+		addSize();
+		updateScore();
+		clearInterval();
+	} 
 
-// 	($slime).width( modWidth);
-// 	modWidth += 0.1;
+}, 100);
 
-// 	($slime).height( modHeight);
-// 	modHeight += 0.1;
-
-// 	$('#score').html(Math.round( $slime.width() * 10 ) / 10);
-
-// 	}, 100);
-// }
+//for loop inside interval to add size per worker. workers.length

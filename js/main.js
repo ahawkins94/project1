516def9c75a9090b261c9902ca61bf1ad0f62adb
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-09-20 23:59:23 +0100

    okay i promise that stress is fixed now for real.

--- js/main.js
+++ js/main.js
@@ -23,9 +23,23 @@ var stressWidth = 0;
 
 function addSize(){
 	($slime).width(modWidth);
+	modWidth += 0.3;
+	($slime).height(modHeight);
+	modHeight += 0.3;
+}
+
+function addSizeStress(){
+	($slime).width(modWidth);
 	modWidth += 0.1;
 	($slime).height(modHeight);
 	modHeight += 0.1;
+}
+
+function addMoreSize(){
+	($slime).width(modWidth);
+	modWidth += 0.6;
+	($slime).height(modHeight);
+	modHeight += 0.6;
 }
 
 function decreaseSize(){
@@ -35,26 +49,30 @@ function decreaseSize(){
 	modHeight -= 0.1;
 }
 
-function increaseStress(){
-	console.log($('.stress'))
+function workerFunction(){
+	
 	$('.stress').each(function(i, v){
-			if ($(this).hasClass('offBench')){
-				var id = $(this).closest('.basic-worker').attr('id');
-				console.log('id: ' + id);
+		var $this = $(this);
+			if ($this.hasClass('offBench')){
+				var id = $this.closest('.basic-worker').attr('id');
 				workerList[id]++;
-				$(this).css("width", "+=0.4px")
+				$this.css("width", "+=0.4px")
+				console.log(workerList[id])
 				if (workerList[id] > 100){
-					workerList[id] = 0;
+					workerList[id] = 100;
 				}
 				if (workerList[id] < 50){
 					addSize();
+				}else if (workerList[id] < 85){
+					addSizeStress();
+				}else if (workerList[id] > 85){
+					decreaseSize();
 				}
 
-			} else if ($(this).hasClass('onBench')){
-				var id = $(this).closest('.basic-worker').attr('id');
-				console.log('id: ' + id);
+			} else if ($this.hasClass('onBench')){
+				var id = $this.closest('.basic-worker').attr('id');
 				workerList[id]--;
-				$(this).css("width", "-=0.4px")
+				$this.css("width", "-=0.4px")
 				if (workerList[id] < 0){
 					workerList[id] = 0;
 				}
@@ -62,8 +80,31 @@ function increaseStress(){
 	})
 }
 
-function increaseStressBar(){
+function feederFunction(){
+	$('.stress').each(function(i, v){
+		var $this = $(this);
+			if ($this.hasClass('offBench')){
+				var id = $this.closest('.slime-feeder').attr('id');
+				workerList[id]++;
+				if (workerList[id] > 100){
+					workerList[id] = 100;
+				}
+				if (workerList[id] < 50){
+					addMoreSize();
+				}else if (workerList[id] < 85){
+					addSize();
+				}else if (workerList[id] > 85){
+					decreaseSize();
+				}
 
+			} else if ($this.hasClass('onBench')){
+				var id = $this.closest('.slime-feeder').attr('id');
+				workerList[id]--;
+				if (workerList[id] < 0){
+					workerList[id] = 0;
+				}
+			}
+	})
 }
 
 function updateScore(){
@@ -110,16 +151,23 @@ $(".feeder-buy").click(function(){
         width: '-=' + buyFeeder + 'px',
         height: '-=' + buyFeeder + 'px'
     }, 500);;
+
+		count++
 		
 		($slime).width(modWidth);
 		modWidth -= buyFeeder;
 
 		($slime).height(modHeight);
 		modHeight -= buyFeeder;
-		$bench.append('<div class="slime-feeder onBench"><div class="stress-bar"><div class="stress"></div></div></div</div>');
+		$bench.append('<div class="slime-feeder onBench" id="w' + count + '"><div class="stress-bar onBench"><div class="stress" label="w' + count + '"></div></div></div</div>');
 		updateScore();
 		buyFeeder = buyFeeder + 30;
 		$(".feeder-buy span").html(buyFeeder)
+
+		workerList['w' + count] = 0;
+		$slimeFeeder = $('.slime-feeder');
+		$stress = $(".stress");
+	
 
 	}
 });
@@ -174,14 +222,14 @@ $(".btn-minimize").click(function(){
 // 	} 
 // }, 300);
 
-setInterval(function(){
-	for (i = 0; i < $workers.find(".slime-feeder").length; i++){
+// setInterval(function(){
+// 	for (i = 0; i < $workers.find(".slime-feeder").length; i++){
 
-		addSize();
-		updateScore();
-		clearInterval();
-	} 
-}, 150);
+// 		addSize();
+// 		updateScore();
+// 		clearInterval();
+// 	} 
+// }, 150);
 
 setInterval(function slimeDecrease(e){
 
@@ -193,8 +241,10 @@ setInterval(function slimeDecrease(e){
 	}, 200);
 
 setInterval(function(){
-		increaseStress();
+		workerFunction()
+		feederFunction();
 		updateScore();
+		clearInterval();
 }, 1000);
 		
 
